e0304e22c3e40d72fdb10a978c42690af794b3dd
Matt Day <mattsday@gmail.com>
2016-07-21 10:05:31 +0100

    Fixed copyright

--- Spark/src/com/cisco/ukidcv/spark/reports/rooms/SparkRoomReportImpl.java
+++ Spark/src/com/cisco/ukidcv/spark/reports/rooms/SparkRoomReportImpl.java
@@ -1,5 +1,6 @@
 /*******************************************************************************
- * Copyright (c) 2016 Matt Day, Cisco and others
+ * Copyright (c) 2016 Cisco and/or its affiliates
+ * @author Matt Day
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
